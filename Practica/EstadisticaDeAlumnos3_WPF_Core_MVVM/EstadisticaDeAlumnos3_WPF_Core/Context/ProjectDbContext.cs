111bb35b969ec65c8b154be30e5417f6cae797f4
Eugene A <eugenax2017@gmail.com>
2020-01-09 12:30:30 +0100

    Cinco

---
+++ Practica/EstadisticaDeAlumnos3_WPF_Core_MVVM/EstadisticaDeAlumnos3_WPF_Core/Context/ProjectDbContext.cs
@@ -0,0 +1,68 @@
+﻿using Common.Lib.Models;
+using Common.Lib.Models.Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstadisticaDeAlumnos3_WPF_Core.Context
+{
+    public class ProjectDbContext : DbContext
+    {
+        public ProjectDbContext()
+        {
+
+        }
+        public ProjectDbContext(DbContextOptions<ProjectDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<StudentSubject> StudentSubjects { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) // 1 variant
+        {
+            optionsBuilder.UseSqlite("Data Source = C:\\Users\\formacio\\Desktop\\Evgenii\\CSharp\\Practica\\EstadisticaDeAlumnos3_WPF_Core_MVVM\\EstadisticaDeAlumnos3_WPF_Core\\bin\\Debug\\netcoreapp3.0\\Test2.db");
+            base.OnConfiguring(optionsBuilder);           
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Entity>()
+                .Ignore(x => x.CurrentValidation);
+
+            builder.Entity<Subject>().
+                Ignore(x => x.StudentSubjects);
+
+            builder.Entity<Student>().
+                Ignore(x => x.StudentSubject);
+
+            //builder.Entity<Examen>()
+            //    .Property(e => e.Date)
+            //    .HasColumnType("date");
+
+            //builder.Entity<Examen>()
+            //    .HasKey(e => new { e.Id, e.Date, e.StudentID, e.SubjectID });
+            //builder.Entity<Examen>()
+            //    .HasOne(e => e.Student)
+            //    .WithMany(e => e.Examens)
+            //    .HasForeignKey(e => e.StudentID);
+            //builder.Entity<Examen>()
+            //    .HasOne(e => e.Subject)
+            //    .WithMany(e => e.Examens)
+            //    .HasForeignKey(e => e.SubjectID);
+            //builder.Entity<StudentSubject>()
+            //    .HasKey(bc => new { bc.StudentId, bc.SubjectId });
+            builder.Entity<StudentSubject>()
+                .HasOne(ss => ss.Student)
+                .WithMany(s => s.StudentSubject)
+                .HasForeignKey(ss => ss.StudentId);
+            builder.Entity<StudentSubject>()
+                .HasOne(ss => ss.Subject)
+                .WithMany(s => s.StudentSubjects)
+                .HasForeignKey(ss => ss.SubjectId);
+        }
+    }
+}
---
+++ Practica/EstadisticaDeAlumnos3_WPF_Core_MVVM/EstadisticaDeAlumnos3_WPF_Core/ViewModels/StudentSubjectViewModel.cs
@@ -0,0 +1,117 @@
+﻿using Common.Lib.Context.Interfaces;
+using Common.Lib.Models;
+using Common.Lib.Models.Core;
+using Common.Lib.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace EstadisticaDeAlumnos3_WPF_Core.ViewModels
+{
+    class StudentSubjectViewModel : ViewModelBase
+    {
+        private StudentSubject selectedStudentSubject;
+        public StudentSubject SelectedStudentSubject
+        {
+            get
+            {
+                return selectedStudentSubject;
+            }
+            set
+            {
+                selectedStudentSubject = value;
+                OnPropertyChanged();
+            }
+        }
+        private string studentDni;
+        public string StudentDni
+        {
+            get
+            {
+                return studentDni;
+            }
+            set
+            {
+                studentDni = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string subjectName;
+        public string SubjectName
+        {
+            get
+            {
+                return subjectName;
+            }
+            set
+            {
+                subjectName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private List<StudentSubject> _studentSubjects;
+        public List<StudentSubject> StudentSubjects
+        {
+            get
+            {
+                return _studentSubjects;
+            }
+            set
+            {
+                _studentSubjects = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public StudentSubjectViewModel()
+        {
+            AddStudentSubjectCommand = new RouteCommand(AddStudentSubject);
+            DeleteStudentSubjectCommand = new RouteCommand(DeleteStudentSubject);
+            GetStudentSubjects();
+        }
+
+        public void GetStudentSubjects()
+        {
+            var repo = Entity.DepCon.Resolve<IRepository<StudentSubject>>();
+
+            StudentSubjects = repo.QueryAll().ToList();
+        }
+
+        public void AddStudentSubject()
+        {
+            var repo1 = Entity.DepCon.Resolve<IRepository<Student>>();
+            var stud = repo1.QueryAll().FirstOrDefault();
+            var repo2 = Entity.DepCon.Resolve<IRepository<Subject>>();
+            var subj = repo2.QueryAll().FirstOrDefault();            
+            StudentSubject newStudent = new StudentSubject { StudentId = stud.Id, SubjectId = subj.Id};           
+            var res_save = newStudent.Save();
+            stud.StudentSubject.Add(newStudent);
+            var res_save1 = stud.Save();
+            if (res_save.IsSuccess && res_save1.IsSuccess) 
+                GetStudentSubjects();
+        }
+
+        public void DeleteStudentSubject()
+        {
+            if (SelectedStudentSubject != null)
+            {
+                SelectedStudentSubject.Delete();
+                GetStudentSubjects();
+            }
+        }
+
+        #region Commands
+        public ICommand AddStudentSubjectCommand { get; set; }
+        public ICommand GetStudentsCommand { get; set; }
+        public ICommand DeleteStudentSubjectCommand { get; set; }
+        public ICommand EditStudentCommand { get; set; }
+        public ICommand ClearStudentCommand { get; set; }
+        public ICommand UpdateStudentCommand { get; set; }
+        #endregion
+
+    }
+}
