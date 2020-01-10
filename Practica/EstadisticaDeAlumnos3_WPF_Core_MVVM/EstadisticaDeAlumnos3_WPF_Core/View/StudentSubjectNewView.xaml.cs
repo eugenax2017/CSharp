caa35a1b52c8bd161ccdaef3f7339d838d7c8c44
Eugene A <eugenax2017@gmail.com>
2020-01-10 12:57:14 +0100

    10/01/20

---
+++ Practica/EstadisticaDeAlumnos3_WPF_Core_MVVM/EstadisticaDeAlumnos3_WPF_Core/View/StudentSubjectNewView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using EstadisticaDeAlumnos3_WPF_Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EstadisticaDeAlumnos3_WPF_Core.View
+{
+    /// <summary>
+    /// Interaction logic for StudentSubjectNewView.xaml
+    /// </summary>
+    public partial class StudentSubjectNewView : UserControl
+    {
+        public StudentSubjectNewView()
+        {
+            InitializeComponent();
+            var vm = new StudentSubjectNewViewModel();
+            this.DataContext = vm;
+        }
+       
+    }
+}
---
+++ Practica/EstadisticaDeAlumnos3_WPF_Core_MVVM/EstadisticaDeAlumnos3_WPF_Core/ViewModels/StudentSubjectNewViewModel.cs
@@ -0,0 +1,93 @@
+﻿using Common.Lib.Context.Interfaces;
+using Common.Lib.Models;
+using Common.Lib.Models.Core;
+using Common.Lib.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace EstadisticaDeAlumnos3_WPF_Core.ViewModels
+{
+    public class StudentSubjectNewViewModel : ViewModelBase
+    {
+        private string dniEntry;
+
+        public string DniEntry
+        {
+            get
+            {
+                return dniEntry;
+            }
+            set
+            {
+                dniEntry = value;
+                if (!string.IsNullOrEmpty(dniEntry))
+                {
+                    var repo = Entity.DepCon.Resolve<IRepository<StudentSubject>>();
+                    SubjectsByStudent = repo.QueryAll().Where(x => x.Student.Dni == dniEntry).Select(x => x.Subject).ToList();
+                }    
+                OnPropertyChanged();
+            }
+        }
+
+        private List<Subject> subjectsByStudent;
+        public List<Subject> SubjectsByStudent
+        {
+            get
+            {                
+                return subjectsByStudent;
+            }
+            set
+            {
+                subjectsByStudent = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private List<Student> _students;
+        public List<Student> Students { 
+            get
+            {
+                //var repo = Entity.DepCon.Resolve<IRepository<Student>>();
+                //_students = repo.QueryAll().ToList();
+                return _students;
+            }
+            set
+            {
+                _students = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public StudentSubjectNewViewModel()
+        {
+
+            AddStudentSubjectCommand = new RouteCommand(AddStudentSubject);
+            GetStudents();
+        }      
+        
+        public void AddStudentSubject()
+        {
+
+        }
+        public void GetStudents()
+        {
+            var repo = Entity.DepCon.Resolve<IRepository<Student>>();
+
+            Students = repo.QueryAll().ToList();
+        }
+
+        #region Commands
+        public ICommand AddStudentSubjectCommand { get; set; }
+        public ICommand GetStudentsCommand { get; set; }
+        public ICommand DeleteStudentSubjectCommand { get; set; }
+        public ICommand EditStudentCommand { get; set; }
+        public ICommand ClearStudentCommand { get; set; }
+        public ICommand UpdateStudentCommand { get; set; }
+        #endregion
+
+    }
+}
