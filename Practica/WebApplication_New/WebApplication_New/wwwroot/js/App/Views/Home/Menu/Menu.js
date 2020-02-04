c71e2430821f72f5c16647ee0efecff16cc940b0
eugenax <eugenax2017@gmail.com>
2020-02-04 13:33:06 +0100

    04/02/20

--- Practica/WebApplication_New/WebApplication_New/wwwroot/js/App/Views/Home/Menu/Menu.js
+++ Practica/WebApplication_New/WebApplication_New/wwwroot/js/App/Views/Home/Menu/Menu.js
@@ -1,1 +1,12 @@
-﻿
+﻿class Menu {
+
+    isNavCollapsed = true;
+
+}
+
+EstadisticaDeAlumnos.
+    component('menu', {
+        templateUrl: './js/App/Views/Home/Menu/Menu.html',
+        controller: ('Menu', Menu),
+        controllerAs: 'vm'
+    });
