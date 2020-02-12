050b281cff8ba0780bba3d59b8f6e70ef9859d62
eugenax <eugenax2017@gmail.com>
2020-02-12 13:05:41 +0100

    12/02/20 13/05

--- Practica/WebApplication_New/WebApplication_New/wwwroot/js/App/EstadisticaDeAlumnos.js
+++ Practica/WebApplication_New/WebApplication_New/wwwroot/js/App/EstadisticaDeAlumnos.js
@@ -1,6 +1,20 @@
 ﻿var EstadisticaDeAlumnos = angular.module('EstadisticaDeAlumnos',
     ['ui.bootstrap',
-        'ui.grid', 'ui.grid.selection']);
+        'ui.grid', 'ui.grid.selection', 'ngRoute']);
 
+EstadisticaDeAlumnos.config(function ($routeProvider) {
+    $routeProvider.when("/", {
+        templateUrl: "default.html"
+    })
+    .when("/students", {
+        templateUrl: "./js/App/Views/Home/Students/StudentsView.html",
+        controller: "StudentsController",
+        controllerAs: 'vm'
+    })
+        .when("/subjects", {
+            templateUrl: "./js/App/Views/Home/Subjects/SubjectsView.html"       
+    });
+});
 
 var Globals = new ClientGlobals();
+
